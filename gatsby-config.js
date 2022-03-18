7ed402b158d8a056f8c2edaef5efdd3f881e657f
appearedherosheep <81283189+appearedherosheep@users.noreply.github.com>
2022-03-18 20:03:55 +0900

    test : Graphql

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,14 +5,14 @@ module.exports = {
     language: "ko",
     // ogImage: '공유할 때 보이는 미리보기 페이지, static하위에 넣기'
   },
-  // plugins: [
-  //   {
-  //     resolve: `gatsby-source-filesystem`,
-  //     options: {
-  //       name: `contents`,
-  //       path: `${__dirname}/contents`,
-  //     },
-  //   },
-  //   `gatsby-transformer-remark`,
-  // ],
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `contents`,
+        path: `${__dirname}/contents`,
+      },
+    },
+    `gatsby-transformer-remark`,
+  ],
 };
